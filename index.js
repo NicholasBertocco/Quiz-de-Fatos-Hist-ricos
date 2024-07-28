0d9eb32f83ae577e5425dda311abc02ba93337bb
Nicholas Bertocco <nicholas.h.bertocco@gmail.com>
2024-07-27 21:42:56 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import input from 'readline-sync';
+
+console.log("------ QUIZ DE FATOS HISTÓRICOS ------");
+
+console.log("\nSeja Bem vindo jogador(a)!\n");
+const name = input.question("Digite deu nome:");
+
